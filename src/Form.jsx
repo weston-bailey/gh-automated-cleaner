befdbc0fb0c55c41dd3d3681e6e6952b458fd163
weston-bailey <wdbailey87@gmail.com>
2022-06-26 21:08:16 -0700

    switching computers

--- src/Form.jsx
+++ src/Form.jsx
@@ -11,24 +11,26 @@ const Form = () => {
 
   // this only runs once
   useEffect(() => {
-    // its bad form to async a useEffect's callback 
-    (async function fetchRepos() {
-      try {
-        // interpolate search into github api url
-        const url = `http://api.github.com/users/${search}/repos`
-        // axios call the API
-        const response = await axios.get(url)
-        // set state conditionally based on response status
-        if(response.status === 200) {
-          console.log(response.data)
-          setRepos(response.data)
-        }
-      } catch(err) {
-        console.log(err)
-      }
-    })() // IIFE async function
+    fetchRepos()
   }, [search])
 
+  const fetchRepos = async () => {
+    try {
+      // interpolate search into github api url
+      const url = `http://api.github.com/users/${search}/repos?per_page=100`
+      // axios call the API
+      const response = await axios.get(url)
+      // set state conditionally based on response status
+      if(response.status === 200) {
+        console.log(response.data)
+        setRepos(response.data)
+      }
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  const handleMoveClick = async e => {}
   // map the user's repos to a div
   const divs = repos.map((repo, idx) => (
           
@@ -37,8 +39,14 @@ const Form = () => {
         target="_blank"
         href={repo.html_url}
       >
-        <p>{repo.name}</p>
-      </a>
+        <p>{idx} {repo.name}</p>
+      </a> 
+
+      <button
+        onClick={handleMoveClick}
+      >
+        move
+      </button>
     </div>
   ))
 
